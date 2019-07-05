b6a6060abb8b9aad24acb620d80a1daa58ab3e4f
Varvara Babkova <varvara.munday@gmail.com>
2019-07-05 19:00:35 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,8 @@
-class AddGradeAndBirthdateToStudent < ActiveRecord::Migration[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
 
     add_column :students, :grade, :integer
-    add_column :students, :birthdate, :string 
+    add_column :students, :birthdate, :string
 
 
   end
